38413ac9b45f5dc7de25f8e17ac62c68b9528e59
Scott-cityboy <scottimbiaka@gmail.com>
2024-02-16 22:34:28 +0300

    Waba Pay

---
+++ payments/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import YourModelName
+
+admin.site.registerlogins
+
---
+++ payments/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class WabaPayConfig(AppConfig):
+    name = 'waba_pay'
+
